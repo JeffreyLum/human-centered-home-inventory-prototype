73a8fc27cd5a0689b71217e166c10b1b3e91ddb1
Jeffrey Lum <jeffreylum81@gmail.com>
2022-04-30 16:33:51 -0700

    asd

---
+++ index.js
@@ -0,0 +1,14 @@
+const t = document.getElementById('couch');
+t.style.display = 'none';
+
+const couch = () => {
+	const couch = document.getElementById('couch');
+
+	couch.style.display = 'block';
+};
+
+const technology = () => {
+	const furniture = document.getElementById('furniture');
+
+	furniture.style.display = 'none';
+};
